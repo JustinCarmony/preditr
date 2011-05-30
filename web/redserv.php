425451a72d790124cd78e251498f7609ebc0445e
justin@justincarmony.com <justin@justincarmony.com>
2011-05-30 12:36:53 -0600

    Redis Interface now fixed

--- web/redserv.php
+++ web/redserv.php
@@ -9,5 +9,45 @@ require_once '../client/Predis.php';
 
 include 'config.php';
 
-// Strip the URI
+try
+{
 
+	// Strip the URI into it's individual parts.
+
+	$uri = str_ireplace($_SERVER['SCRIPT_NAME'], '', $_SERVER['REQUEST_URI']);
+
+	// trim off any slashes
+	$uri = trim($uri, '/');
+
+	$parts = explode('/', $uri);
+
+	$arguments = array();
+	$method = $parts[0];
+
+	if(count($parts) > 1)
+	{
+		$arguments = array_slice($parts, 1);
+		
+	}
+
+	$predis = new Predis_Client($config['predis']);
+
+	$return = call_user_func(array($predis, $method), $arguments);
+}
+catch(Exception $ex)
+{
+	header("HTTP/1.1 500 Internal Server Error");
+	$obj = new stdClass();
+	$obj->success = false;
+	$obj->err_msg = $ex->getMessage();
+	echo json_encode($obj);
+	exit();
+}
+
+$obj = new stdClass();
+$obj->success = true;
+$obj->return = $return;
+
+echo json_encode($obj);
+
+exit();
